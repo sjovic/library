cf1b6b89f8595daaa76659682fb90f4f119ee66d
Marko Matic <maticmarkozr@gmail.com>
2017-05-29 17:31:47 +0200

    Use directives

--- src/main/resources/static/js/controllers/bookCategoryController.js
+++ src/main/resources/static/js/controllers/bookCategoryController.js
@@ -6,7 +6,6 @@ app.controller('BookCategoryController', function($scope, bookCategoryService) {
 
     $scope.headingTitle = "Book";
     
-    //Get all menus
     var handleSuccessCategory = function(data, status){
     	$scope.categories = data;
     }
@@ -14,35 +13,5 @@ app.controller('BookCategoryController', function($scope, bookCategoryService) {
     	bookCategoryService.getCategories().then(handleSuccessCategory);
     }
     getCategories();
-    
-    //Create new menu
-    $scope.category = {};
-    $scope.saveCategory = function(category){
-    	console.log(category);
-    	bookCategoryService.createCategory(category).then(function(response){
-    	getCategories();
-    	}, function(error){
-    		
-    	})
-    	//remove input value after submit
-    	$scope.category = null;
-    }
-   
-    
-    //edit menu
-    $scope.editCategory = function(category){
-    	console.log(category);
-
-    }
-    
-    //remove menu
-    $scope.deleteCategory = function(id){
-    	console.log(id);
-    	bookCategoryService.deleteCategory(id).then(function(response){
-    	getCategories();
-    	}, function(error){
-    		
-    	});
-    }
 
 });
---
+++ src/main/resources/static/js/directives/categoryDirective.js
@@ -0,0 +1,47 @@
+angular.module('book.directives')
+.directive('categoryDirective', function() {
+	  return {
+		restrict: 'E',
+	    templateUrl: 'views/category.html',
+	    controller : ['$scope', 'bookCategoryService', function ($scope, bookCategoryService) {
+	    	//Get all menus
+	        var handleSuccessCategory = function(data, status){
+	        	$scope.categories = data;
+	        }
+	        var getCategories = function(){
+	        	bookCategoryService.getCategories().then(handleSuccessCategory);
+	        }
+	    	//Create new menu
+	        $scope.category = {};
+	        $scope.saveCategory = function(category){
+	        	console.log(category);
+	        	bookCategoryService.createCategory(category).then(function(response){
+	    	        getCategories();
+	        	}, function(error){
+	        		
+	        	})
+	        	//remove input value after submit
+	        	$scope.category = null;
+	        }
+	       
+	        //edit menu
+	        $scope.editCategory = function(category){
+	        	console.log(category);
+
+	        }
+	        
+	        //remove menu
+	        $scope.deleteCategory = function(id){
+	        	console.log(id);
+	        	bookCategoryService.deleteCategory(id).then(function(response){
+	        	    getCategories();
+	        	}, function(error){
+	        		
+	        	});
+	        }
+	          
+
+	          
+	      }]
+	  };
+	});
