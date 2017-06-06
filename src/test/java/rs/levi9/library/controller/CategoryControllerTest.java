fe32c93919556e44bcd0f9c20485aa6b6caa094b
Marko Matic <ma.matic@levi9.com>
2017-06-06 09:52:20 +0200

    Add setUp method in unit tests

--- src/test/java/rs/levi9/library/controller/CategoryControllerTest.java
+++ src/test/java/rs/levi9/library/controller/CategoryControllerTest.java
@@ -2,8 +2,11 @@ package rs.levi9.library.controller;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -23,16 +26,25 @@ public class CategoryControllerTest {
 	@Mock
 	private CategoryService categoryService;
 	
+	private Category category;
+	
+	@Before
+    public void setUp() throws Exception {
+		category = new Category();
+		category.setId(1L);
+		category.setName("Java");
+	}
+	
 	@Test
 	public void shouldFindOneCategory(){
-		Category category = new Category();
-		category.setId(1L);
-        
+		
 	    when(categoryService.findOne(1L)).thenReturn(category);
 
 	    ResponseEntity<Category> resultCategory = categoryController.findOne(1L);
 	    
 	    assertNotNull(resultCategory);
 	    assertEquals(resultCategory.getBody(), category);
+	    
+	    verify(categoryService, times(1)).findOne(1L);
     }
 }
--- src/test/java/rs/levi9/library/service/BookServiceTest.java
+++ src/test/java/rs/levi9/library/service/BookServiceTest.java
@@ -4,6 +4,9 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.mockito.Mockito.when;
 
+import java.util.Date;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -11,6 +14,7 @@ import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import rs.levi9.library.domain.Book;
+import rs.levi9.library.domain.Category;
 import rs.levi9.library.repository.BookRepository;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -22,17 +26,34 @@ public class BookServiceTest {
 	@Mock
 	BookRepository bookRepository;
 	
+	private Category category;
+	private Book book;
+	
+	@Before
+    public void setUp() throws Exception {
+		category = new Category();
+		category.setId(1L);
+		category.setName("Java");
+		
+		book = new Book();
+		book.setId(1L);
+		book.setCategory(category);
+		book.setName("Thinking in java");
+		book.setNumberOfPages(445);
+		book.setAuthor("Bruce Eckel");
+		book.setPublishDate(new Date());
+		book.setIsbn("8175257660");
+	}
+	
 	@Test
 	public void shouldSaveBook(){
-		Book book = new Book();
-		book.setName("Book Name");
 		
 		when(bookRepository.save(book)).thenReturn(book);
 		
 		Book savedCategory = bookService.save(book);
 		
 		assertNotNull(savedCategory);
-		assertEquals(savedCategory.getName(), "Book Name");
+		assertEquals(savedCategory.getName(), "Thinking in java");
     }
 
 }
