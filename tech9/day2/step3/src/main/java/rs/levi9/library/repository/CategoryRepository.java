9a020cf909d5b598e86ed6d1768ae57d51b5ab50
Strahinja Jovic <s.jovic@levi9.com>
2017-07-05 09:07:45 +0200

    add validation

--- tech9/day2/step3/src/main/java/rs/levi9/library/repository/CategoryRepository.java
+++ tech9/day2/step3/src/main/java/rs/levi9/library/repository/CategoryRepository.java
@@ -7,4 +7,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 
+    public Category findByName(String name);
+
 }
--- tech9/day2/step3/src/main/resources/rs/levi9/library/sql/data.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO `category`(`name`) VALUES('Java');
-INSERT INTO `category`(`name`) VALUES('Spring');
-INSERT INTO `category`(`name`) VALUES('Data Structures');
-
-INSERT INTO `book` (`isbn`, `name`, `author`, `publish_date`, `number_of_pages`, `category_id`)
-    VALUES ('978-86-7555-308-3', 'Thinking In Java', 'Bruce Eckel', '2007-01-01', 1177, 1);
-INSERT INTO `book` (`isbn`, `name`, `author`, `publish_date`, `number_of_pages`, `category_id`)
-    VALUES ('978-46-7353-511-4', 'Spring In Action', 'Craig Walls', '2014-01-01', 483, 2);
