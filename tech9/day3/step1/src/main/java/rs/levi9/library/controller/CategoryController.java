59d50301b7920a95148f5abddd9291f3be66489c
Strahinja Jovic <s.jovic@levi9.com>
2017-07-06 16:07:50 +0200

    start day3, step1

---
+++ tech9/day3/step1/src/main/java/rs/levi9/library/controller/CategoryController.java
@@ -0,0 +1,70 @@
+package rs.levi9.library.controller;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import rs.levi9.library.controller.validation.CategoryValidator;
+import rs.levi9.library.domain.Category;
+import rs.levi9.library.service.CategoryService;
+
+@RestController
+@RequestMapping("/categories")
+public class CategoryController {
+
+    private CategoryService categoryService;
+    private CategoryValidator categoryValidator;
+
+    @Autowired
+    public CategoryController(CategoryService categoryService, CategoryValidator categoryValidator) {
+        this.categoryService = categoryService;
+        this.categoryValidator = categoryValidator;
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public List<Category> findAll() {
+        return categoryService.findAll();
+    }
+
+    @RequestMapping(path = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity findOne(@PathVariable("id") Long id) {
+        Category category = categoryService.findOne(id);
+        if (category == null) {
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity(category, HttpStatus.OK);
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public Category save(@RequestBody Category category, BindingResult result) throws MethodArgumentNotValidException {
+        categoryValidator.validate(category, result);
+        if (result.hasErrors()) {
+            throw new MethodArgumentNotValidException(null, result);
+        }
+        return categoryService.save(category);
+    }
+
+    @RequestMapping(path = "{id}", method = RequestMethod.DELETE)
+    public ResponseEntity remove(@PathVariable("id") Long id) {
+        try {
+            categoryService.delete(id);
+        } catch (EmptyResultDataAccessException e) {
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity(HttpStatus.OK);
+    }
+    
+    @RequestMapping(method = RequestMethod.PUT)
+    public Category edit(@RequestBody Category category) {
+        return categoryService.save(category);
+    }
+
+}
---
+++ tech9/day3/step1/src/main/resources/static/js/controllers/bookController.js
@@ -0,0 +1,108 @@
+/**
+ * 
+ */
+
+(function(){
+angular.module('app')
+    .controller('BookController', BookController);
+    
+    BookController.$inject = ['$filter', 'CategoryService', 'BookService', 'uibDateParser'];
+   
+    function BookController($filter, CategoryService, BookService, uibDateParser) {
+        
+        var vm = this;
+        vm.addBook = addBook;
+        vm.cancelForm = cancelForm;
+        vm.clearBook = clearBook;
+        vm.deleteBook = deleteBook;
+        vm.editBook = editBook;
+        vm.openCalendar = openCalendar;
+        vm.saveBook = saveBook;
+        
+        vm.showForm = false;
+
+        init();
+
+        function init() {
+            getCategories();
+            getBooks();
+            //Create new book
+            vm.book = {
+                publishDate: new Date()
+            };
+        }
+
+        vm.datePickerOptions = {
+            formatYear: 'yy',
+            maxDate : new Date()
+        };
+
+        vm.popupCalendar = {
+           opened: false
+        }; 
+
+        function addBook(){
+            vm.showForm = true;
+            init();
+        }
+        
+        function cancelForm(){
+            vm.showForm = false;
+        }
+        
+        function clearBook(){
+            vm.addBookForm.$setPristine();
+            vm.book = {};
+        }
+        
+        function deleteBook(id){
+            console.log(id);
+            BookService.deleteBook(id).then(function(response){
+                getBooks();
+            }, function(error){
+
+            });
+        }
+
+        function editBook(book){
+            vm.showForm = true;
+            vm.book = angular.copy(book);
+            vm.book.publishDate = new Date(vm.book.publishDate.split('-').join(' '));
+        }
+
+        function getCategories(){
+            CategoryService.getCategories().then(handleSuccessCategories);
+        }
+        
+        function getBooks(){
+            BookService.getBooks().then(handleSuccessBooks);
+        }
+
+        //Get all category
+        function handleSuccessCategories(data, status){
+            vm.categories = data;
+        }
+        
+        //Get all books
+        function handleSuccessBooks(data, status){
+            vm.books = data.data;
+        }
+
+        function openCalendar() {
+            vm.popupCalendar.opened = true;
+        };
+
+        function saveBook(book){
+            book.publishDate = $filter('date')(book.publishDate, "yyyy-MM-dd");
+            console.log(book.publishDate);
+            BookService.saveBook(book).then(function(response){
+                getBooks();
+            }, function(error){
+
+            })
+            //remove input value after submit
+            vm.addBookForm.$setPristine();
+            vm.showForm = false;
+        }
+    };
+})();
